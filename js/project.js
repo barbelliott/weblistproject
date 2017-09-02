2844b83d279cc0b458f0b22b9e9d3f012dedc856
barbelliott <barbelliott1@comcast.net>
2017-09-01 22:45:39 -0700

    a few fixes

--- js/project.js
+++ js/project.js
@@ -1,4 +1,4 @@
-function nameSearchFunction () {
+/*function nameSearchFunction () {
     var input, filter, table, tr, td, i;
     input = document.getElementById("nameInput");
     filter = input.value.toUpperCase();
@@ -17,11 +17,11 @@ function nameSearchFunction () {
       }
     } 
   }
-}
+}*/
 
  /*backup of original before trying to make it search only the name column*/
 
-/*function nameSearchFunction () {
+function nameSearchFunction () {
   var input, filter, table, tr, td, i;
   input = document.getElementById("nameInput");
   filter = input.value.toUpperCase();
@@ -29,8 +29,32 @@ function nameSearchFunction () {
   tr = table.getElementsByTagName("tr");
 
   // Loop through all table rows, and hide those who don't match the search query
+/*  document.getElementById('table1').rows[rowIndex].cells[cellIndex];*/
+    
   for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[0];
+    td = tr[i].getElementsByTagName("td")[0]; /*the [0] might mean it's only checking the first column!*/
+    if (td) {
+      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    } 
+  }
+}
+
+/*function called when some types into the Key search box*/
+/*function keySearchFunction () {
+    var input, filter, table, tr, td, i;
+    input = document.getElementById("keyInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("tuneTable");
+    tr = table.getElementsByTagName("tr");
+    td = tr[1];
+
+  // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[1];
     if (td) {
       if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
         tr[i].style.display = "";
@@ -41,28 +65,6 @@ function nameSearchFunction () {
   }
 }*/
 
-/*function called when some types into the Key search box*/
-function keySearchFunction () {
-    var input, filter, table, tr, td, i;
-    input = document.getElementById("keyInput");
-    filter = input.value.toUpperCase();
-    table = document.getElementById("tuneTable");
-    tr = table.getElementsByTagName("tr");
-    td = tr[1];
-
-  // Loop through all table rows, and hide those who don't match the search query
-    for (i = 0; i < tr.length; i++) {
-    td = tr[i].firstElementChild;
-    if (td) {
-      if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-        tr[i].style.display = "";
-      } else {
-        tr[i].style.display = "none";
-      }
-    } 
-  }
-}
-
 /*sudo code for filtering a single comumn
 for every row in tuneTable
 go get the first row
@@ -70,3 +72,58 @@ look in the first cell (the name column)
 match it with the name
 if it matches, show the row
 */
+
+function sortTable(n) {
+  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+  table = document.getElementById("tuneTable");
+  switching = true;
+  //Set the sorting direction to ascending:
+  dir = "asc"; 
+  /*Make a loop that will continue until
+  no switching has been done:*/
+  while (switching) {
+    //start by saying: no switching is done:
+    switching = false;
+    rows = table.getElementsByTagName("TR");
+    /*Loop through all table rows (except the
+    first, which contains table headers):*/
+    for (i = 1; i < (rows.length - 1); i++) {
+      //start by saying there should be no switching:
+      shouldSwitch = false;
+      /*Get the two elements you want to compare,
+      one from current row and one from the next:*/
+      x = rows[i].getElementsByTagName("TD")[n];
+      y = rows[i + 1].getElementsByTagName("TD")[n];
+      /*check if the two rows should switch place,
+      based on the direction, asc or desc:*/
+      if (dir == "asc") {
+        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+          //if so, mark as a switch and break the loop:
+          shouldSwitch= true;
+          break;
+        }
+      } else if (dir == "desc") {
+        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+          //if so, mark as a switch and break the loop:
+          shouldSwitch= true;
+          break;
+        }
+      }
+    }
+    if (shouldSwitch) {
+      /*If a switch has been marked, make the switch
+      and mark that a switch has been done:*/
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+      //Each time a switch is done, increase this count by 1:
+      switchcount ++; 
+    } else {
+      /*If no switching has been done AND the direction is "asc",
+      set the direction to "desc" and run the while loop again.*/
+      if (switchcount == 0 && dir == "asc") {
+        dir = "desc";
+        switching = true;
+      }
+    }
+  }
+}
